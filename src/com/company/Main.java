33c86bfba49959a6cf555eba0b3ca8fe0a6a2eaa
aaronvictor <aaron.victor30@gmail.com>
2022-06-05 21:41:34 -0400

    Lab4_Victor_Aaron

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,60 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Variables
+        int intOperandA = 1;
+       int intOperandB = 2;
+       int intSum = 3;
+       int intProduct = 7;
+       int intDifference = 0;
+       int intQuotient = 20;
+       int intModulo = 16;
+
+        intSum = intOperandA + intOperandB;
+        intDifference = intOperandB - intOperandA;
+        intProduct = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+
+        System.out.println(intOperandA + intOperandB);
+        System.out.println(intOperandA - intOperandB);
+        System.out.println(intOperandA * intOperandB);
+        System.out.println(intOperandA / intOperandB);
+        System.out.println(intOperandA % intOperandB);
+
+
+       double doubleOperaA = 2.1;
+       double doubleOperaB = 3.1;
+       double doubleSum = 5.2;
+        double doubleProduct = 13.2;
+       double doubleDifference = 6.7;
+       double doubleQuotient = 5.3;
+
+
+        doubleSum = doubleOperaA + doubleOperaB;
+        doubleProduct = doubleOperaA - doubleOperaB;
+        doubleDifference = doubleOperaB * doubleOperaA;
+        doubleQuotient = doubleOperaA % doubleOperaB;
+
+        String txt = "Double Values";
+        System.out.println(txt.toUpperCase());
+
+        System.out.println(doubleOperaA + doubleOperaB);
+        System.out.println(doubleOperaA - doubleOperaB);
+        System.out.println(doubleOperaA * doubleOperaB);
+        System.out.println(doubleOperaA / doubleOperaB);
+
+       // Variables
+        int familyKids = 2;
+        boolean rain = false;
+        double gasPrice = 4.99;
+        int favNumber = 7;
+        int shoeSize = 10;
+        int birthMonth = 12;
+
+
+
+
+
     }
 }
